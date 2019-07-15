a1aa4cef92f4280205a8671bf993ceca28bc6f6b
mino <lv.mitsuru@gmail.com>
2019-07-16 01:04:05 +0900

    add go-restapi-sample

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+
+	// Echoのインスタンス作る
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// ルーティング
+	e.GET("/hello", hello)
+
+	// サーバー起動
+	e.Start(":1323")
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
